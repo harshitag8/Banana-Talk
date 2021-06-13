e03ec0b909724c5f39c8be9e71db96e7d4e0a212
Harshit Agrawal <yourharshitagrawal@gmail.com>
2021-06-13 17:06:02 +0530

    ex06>till end

--- app.js
+++ app.js
@@ -9,16 +9,40 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output")
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
-console.log(outputDiv);
+var serverURL =  "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server, try after some time")
+}
+
+
+
+// console.log(txtInput);
+// console.log(outputDiv);
 
 function clickHandler() {
-    console.log("Clicked!!");
-    console.log("input", txtInput.value);
+    // console.log("Clicked!!");
+    // console.log("input", txtInput.value);
 
-    outputDiv.innerText = txtInput.value;
+    // outputDiv.innerText = "ahxj" txtInput.value;
+
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+         .then(response => response.json())
+         .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            })
+         .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
